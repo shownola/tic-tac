3f406f87a0a9049004ff17563107bc339b752cdc
shownola <swasieko@gmail.com>
2021-01-04 13:51:40 -0700

    Fix * in simpleForm, set player actions in pages_controller, edit routes and styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'pages#home'
-  get 'pages/game'
-  get 'pages/hiscores'
+  get 'game', to: 'pages#game'
+  get 'hiscores', to: 'pages#hiscores'
   post '/played', to: 'pages#played'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
