41d0e854f3c96da5b50eacdcf158fdb09d27c5b7
shownola <swasieko@gmail.com>
2021-01-04 11:21:56 -0700

    Add bootstrap, simple_form, devise, pages controller and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root 'pages#home'
+  get 'pages/game'
+  get 'pages/hiscores'
+  post '/played', to: 'pages#played'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
